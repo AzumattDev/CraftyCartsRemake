15ec2dabc512f7e7fc8a5473ec5806587b64b795
AzumattDev <azumattgaming@gmail.com>
2022-06-05 23:46:24 -0400

    ver bump 3.0.6

--- MainPlugin.cs
+++ MainPlugin.cs
@@ -21,7 +21,7 @@ namespace CraftyCartsRemake
          * It now uses the PieceManager written by me to load the carts into the world. I rebuilt the assets inside unity to have scripts there, and not having everything done inside the code.
          * This makes it a bit less buggy on the material and works more fluidly. Rolopogo is credited in the AssemblyInfo.cs file, and here. Thank you again Rolo.
          */
-        public const string ModVersion = "3.0.5";
+        public const string ModVersion = "3.0.6";
         public const string ModGUID = "azumatt.CraftyCarts";
         public const string Author = "Azumatt";
         public const string ModName = "CraftyCarts";
